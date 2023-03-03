b53d4fb9cceb6ab8673a7c2f88546ecea865033e
Bohrzz <113268086+Bohrzz@users.noreply.github.com>
2023-03-02 21:53:25 -0500

    fixed server issues

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>V 0.1 by Bohrzz</p>
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = ({title}) => {
+  return (
+    <header>
+        <h2>Welcome to Jhans porfoliooo</h2>
+        <h4>You are on the {title}page</h4>
+    </header>
+  )
+}
+
+export default Header
